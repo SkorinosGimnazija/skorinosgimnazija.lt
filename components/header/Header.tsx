d64a290ad3b55ec4b604d7818f88ce34e6636c27
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-03-17 11:59:38 +0200

    header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,48 +1,36 @@
-import { GetStaticProps } from 'next';
-import Image from 'next/image';
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import HeroImage from '../../assets/images/hero.jpg';
 import LogoImage from '../../assets/images/logo3.png';
 
 export const Header: React.FC = () => {
   return (
-    <header className="relative h-[350px] w-full">
-      <div className="absolute left-0 right-0 flex min-h-[4rem] w-full flex-wrap items-center  px-4 text-white backdrop-blur-sm">
-        <h1 className="text-shadow mr-4 min-w-fit text-xl">
-          Vilniaus Pranciškaus Skorinos gimnazija
-        </h1>
-        <div className="text-shadow flex flex-1 justify-end gap-3 text-lg">
-          <ul className="flex gap-3">
-            <li>LT</li>
-            <li>BY</li>
-            <li>EN</li>
-          </ul>
-          <span>A</span>
-          <span>B</span>
-        </div>
+    <section
+      className="relative h-[350px] bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${HeroImage.src})` }}
+    >
+      <div className="backdrop-blur-sm">
+        <header className="flex min-h-[5rem] flex-wrap items-center px-4 text-white">
+          <h1 className="text-shadow mr-4 min-w-fit text-2xl">
+            Vilniaus Pranciškaus Skorinos gimnazija
+          </h1>
+          <div className="text-shadow flex flex-1 justify-end gap-3 text-lg">
+            <ol className="flex gap-3">
+              <li>LT</li>
+              <li>BY</li>
+              <li>EN</li>
+            </ol>
+            <span>A</span>
+            <span>B</span>
+          </div>
+        </header>
       </div>
-      {/* <span className="absolute top-1/2 left-1/2 -translate-y-1/3 -translate-x-1/2 sm:left-52">
-        <Image src={LogoImage} alt="logo" width="150" height="150" quality="100" />
-      </span> */}
       <img
         className="absolute top-1/2 left-1/2 -translate-y-1/3 -translate-x-1/2 sm:left-52"
         src={LogoImage.src}
         alt="logo"
-        width="150"
-        height="150"
+        width="175"
+        height="175"
       />
-      <img
-        className="h-full w-full object-cover"
-        src={HeroImage.src}
-        alt="Vilniaus Pranciškaus Skorinos gimnazija"
-      />
-      {/* <Image
-          src={HeroImage}
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          alt="Vilniaus Pranciškaus Skorinos gimnazija"
-        /> */}
-    </header>
+    </section>
   );
 };
