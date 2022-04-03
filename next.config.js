712a8e41229184d4309c0c98ee6cd093f2bba1f3
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-04-04 02:40:45 +0300

    security headers

--- next.config.js
+++ next.config.js
@@ -23,4 +23,33 @@ module.exports = {
       ],
     };
   },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ];
+  },
 };
