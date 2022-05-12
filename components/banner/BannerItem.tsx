e9dd266233e03481c0092cdab4996a3909d05c0d
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-05-12 19:01:09 +0300

    banner item

---
+++ components/banner/BannerItem.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { IBanner } from '../../models/models';
+
+interface Props {
+  banner: IBanner;
+}
+
+export const BannerItem: React.FC<Props> = ({ banner }) => {
+  const isLocal = banner.url.startsWith('/');
+
+  return (
+    <div className="my-4 hidden overflow-hidden rounded-lg bg-white bg-opacity-70 px-2 py-4 shadow-md backdrop-blur-lg lg:block">
+      <Link href={banner.url}>
+        <a
+          target={isLocal ? null : '_blank'}
+          rel={isLocal ? null : 'noreferrer noopener'}
+          title={banner.title}
+        >
+          <Image
+            className="mx-auto transition-transform duration-300 hover:scale-105"
+            src={`${process.env.NEXT_PUBLIC_STATIC_URL}/${banner.pictureUrl}`}
+            width={banner.width}
+            height={banner.height}
+            alt={banner.title}
+          />
+        </a>
+      </Link>
+    </div>
+  );
+};
--- components/banner/BannerList.tsx
+++ components/banner/BannerList.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
 import React from 'react';
 import { IBanner } from '../../models/models';
+import { BannerItem } from './BannerItem';
 
 interface Props {
   banners?: IBanner[];
@@ -14,24 +14,7 @@ export const BannerList: React.FC<Props> = ({ banners }) => {
   return (
     <div className="mt-8">
       {banners?.map((x) => {
-        return (
-          <div
-            key={x.id}
-            className="my-4 hidden overflow-hidden rounded-lg bg-white bg-opacity-70 px-2 py-4 shadow-md backdrop-blur-lg lg:block"
-          >
-            <Link href={x.url}>
-              <a target="_blank" rel="noreferrer noopener" title={x.title}>
-                <img
-                  className="mx-auto transition-transform duration-300 hover:scale-105"
-                  src={`${process.env.NEXT_PUBLIC_STATIC_URL}/${x.pictureUrl}`}
-                  width={x.width}
-                  height={x.height}
-                  alt={x.title}
-                />
-              </a>
-            </Link>
-          </div>
-        );
+        return <BannerItem key={x.id} banner={x} />;
       })}
     </div>
   );
