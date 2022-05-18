832e734015168bc9d5f9dd643f2c10ff2e6e2afe
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-05-18 11:35:00 +0300

    theme color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#fde047" />
       </Head>
 
       <Component {...pageProps} />
