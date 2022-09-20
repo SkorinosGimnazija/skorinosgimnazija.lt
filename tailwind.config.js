3267e9ce5c4727fee414692e76e399a70a95d9bb
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-09-20 05:33:03 +0300

    temp fix for edge container

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,14 @@ module.exports = {
     },
     container: {
       center: true,
+      screens: {
+        // temp ?
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1500px',
+      },
     },
   },
   safelist: [
