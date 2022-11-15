0d47e42f2b4e39b8fa9a4642313591d36628ba6c
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-11-16 01:08:00 +0200

    youtube fix

--- components/markdown/widgets/Youtube.tsx
+++ components/markdown/widgets/Youtube.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 interface Props {
-  children: string;
+  children: [string];
 }
 
 export const Youtube: React.FC<Props> = ({ children }) => {
   return (
     <div className="flex w-full justify-center">
       <iframe
-        src={`https://www.youtube-nocookie.com/embed/${children}`}
-        className="aspect-video h-96 max-w-full"
+        src={`https://www.youtube-nocookie.com/embed/${children[0]}`}
+        className="aspect-video w-full max-w-full pt-6"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
