376e2a8bbf41afe6b2172df7578a735064e09503
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-11-26 04:44:25 +0200

    yt margin

--- components/markdown/widgets/Youtube.tsx
+++ components/markdown/widgets/Youtube.tsx
@@ -6,10 +6,10 @@ interface Props {
 
 export const Youtube: React.FC<Props> = ({ children }) => {
   return (
-    <div className="flex w-full justify-center">
+    <div className="mt-8 flex w-full justify-center">
       <iframe
         src={`https://www.youtube-nocookie.com/embed/${children[0]}`}
-        className="aspect-video w-full max-w-full pt-6"
+        className="aspect-video w-full max-w-full"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
