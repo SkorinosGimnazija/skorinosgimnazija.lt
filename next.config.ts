fa65bf41d107cc866fd4578a45ab78fc351d5568
eXmaS <76765030+eXmaS@users.noreply.github.com>
2026-02-06 16:28:59 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: 'standalone',
   compress: false,
   reactStrictMode: true,
   poweredByHeader: false,
   experimental: {
-    scrollRestoration: true
+    scrollRestoration: true,
   },
   images: {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'localhost'
+        hostname: 'localhost',
       },
       {
         protocol: 'https',
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
     ],
     qualities: [75, 80, 90, 95, 100],
     contentDispositionType: 'inline',
-    dangerouslyAllowLocalIP: true
+    dangerouslyAllowLocalIP: process.env.NODE_ENV === 'development',
   },
   i18n: {
     locales: ['lt', 'by', 'en'],
@@ -45,20 +45,16 @@ const nextConfig: NextConfig = {
         source: '/:path*',
         headers: [
           {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
           {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
+            value: 'strict-origin-when-cross-origin',
           },
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self';",
+            value: 'frame-ancestors \'self\';',
           },
         ],
       },
