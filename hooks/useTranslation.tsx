871ed44d61acb4e69ff5cdd51e593ce5ebdaf3c8
eXmaS <76765030+eXmaS@users.noreply.github.com>
2026-08-19 08:35:22 +0300

    del

--- hooks/useTranslation.tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router';
-import { translation } from '../translation/i18n';
-
-export const useTranslation = () => {
-  const { locale } = useRouter();
-  return { t: translation[locale!], locale };
-};
